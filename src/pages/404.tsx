6dea4efe76e158fa46083e7ac8db03cf80901f15
Louis <6723574+louisgv@users.noreply.github.com>
2022-06-23 20:37:30 -0400

    Update doc 404, fix up some detail

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,24 +2,24 @@ import Link from "next/link"
 
 export default function NotFoundPage() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh"
-      }}>
-      <h1
-        style={{
-          fontSize: "1.5rem",
-          marginBottom: "1rem"
-        }}>
-        404 | where no one has gone before
-      </h1>
+    <article className="flex flex-col items-center items-center justify-center p-8">
+      <h1>404</h1>
+      <h2>Where no one has gone before</h2>
+      <blockquote>
+        <p>
+          We're the same. We share the same history, the same heritage, the same
+          lives. We're tied together beyond any untying. Man or woman, it makes
+          no difference. We're human. We couldn't escape from each other even if
+          we wanted to. That's how you do it, by remembering who and what you
+          are - a bit of flesh and blood afloat in a universe without end. The
+          only thing that's truly yours is the rest of humanity.
+        </p>
+      </blockquote>
       <Link href="/">
-        <a>👈 Back to base</a>
+        <a className="w-full text-lg font-medium p-4 no-underline transition-colors text-gray-600 dark:text-gray-300 dark:hover:text-primary-500 hover:text-primary-500 inline-flex items-center justify-center rounded ml-2">
+          👈 Back to base
+        </a>
       </Link>
-    </div>
+    </article>
   )
 }
