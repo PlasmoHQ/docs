b1b04febc634702fecdc1f219f1567db02920aee
Louis <6723574+louisgv@users.noreply.github.com>
2022-08-16 18:24:45 +0100

    Adding shortcut to github issue creation

--- next.config.js
+++ next.config.js
@@ -30,6 +30,21 @@ module.exports = withPlausibleProxy()(
           source: "/csui",
           destination: "/workflows/content-scripts-ui",
           permanent: true
+        },
+        {
+          source: "/exp",
+          destination: "https://github.com/PlasmoHQ/plasmo/issues/new?assignees=&labels=documentation&template=2.example.yml&title=%5BEXP%5D+",
+          permanent: true
+        },
+        {
+          source: "/rfc",
+          destination: "https://github.com/PlasmoHQ/plasmo/issues/new?assignees=&labels=enhancement&template=0.rfc.yml&title=%5BRFC%5D+",
+          permanent: true
+        },
+        {
+          source: "/bug",
+          destination: "https://github.com/PlasmoHQ/plasmo/issues/new?assignees=&labels=bug%2Ctriage&template=1.bug.yml&title=%5BBUG%5D+",
+          permanent: true
         }
       ]
     }
